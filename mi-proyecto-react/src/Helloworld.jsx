ee1a42d1495f0223f36a5798b10bc1c3741e5f68
Emiliano-Herrera <emilianoherrera701@gmail.com>
2026-03-09 07:14:48 -0300

    Sprint 1, practico 01.

---
+++ mi-proyecto-react/src/Helloworld.jsx
@@ -0,0 +1,15 @@
+// importo el css
+import "./HelloWorld.css";
+
+function HelloWorld() {
+  return (
+    <>
+      <div className="hello-container">
+        <h1 className="hello-title">¡Hola, Mundo!</h1>
+        <p className="hello-subtitle">Mi primer componente en React - aguante Boquita</p>
+      </div>
+    </>
+  );
+}
+
+export default HelloWorld;
