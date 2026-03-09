e41b54ed9059081ca520f165f0f2e734288e5b99
Emiliano-Herrera <emilianoherrera701@gmail.com>
2026-03-09 08:00:59 -0300

    Sprint 1, practico 01 - update.

--- mi-proyecto-react/src/App.jsx
+++ mi-proyecto-react/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import HelloWorld from './Helloworld'
+import HelloWorld from './components/HelloWorld'
 
 function App() {
   const [count, setCount] = useState(0)
